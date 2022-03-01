9b710eda9e0bd129a5b8d30fbaa14008bdd87d4f
manojkumar4799 <manoj.mutukuloju18@gmail.com>
2022-03-01 18:46:49 +0530

    created enemy, enemy script and enemy patrolling+ani

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] bool movingRight = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyPatrol();
+
+    }
+
+    private void EnemyPatrol()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D ray = Physics2D.Raycast(groundCheck.position, Vector2.down, 1f);
+        if (ray.collider == false)
+        {
+            if (movingRight)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                movingRight = true;
+            }
+        }
+    }
+}
